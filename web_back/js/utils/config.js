a29544eeb0bb402d11104d21ad5ee9c413584445
fanyoufu <fanyoufu@outlook.com>
2019-09-24 09:16:02 +0800

    实现了文章列表的基本显示

--- web_back/js/utils/config.js
+++ web_back/js/utils/config.js
@@ -7,5 +7,7 @@ var URLIST = {
   category_show: BASE_URL + '/admin/category_search',
   category_add: BASE_URL + '/admin/category_add',
   category_del: BASE_URL + '/admin/category_delete',
-  category_edit: BASE_URL + '/admin/category_edit'
+  category_edit: BASE_URL + '/admin/category_edit',
+
+  article_show: BASE_URL + '/admin/search'
 };
